78a19e22e745745311155d55a2018d9d19109e4b
= <=>
2022-10-13 10:48:05 -0300

    feat : Implementação dos metodos principais

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -4,15 +4,34 @@ namespace App\Http\Livewire;
 
 use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um testes'; 
+    use WithPagination;
+
+    public $content = 'Apenas um testes'; 
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
-        $tweets = Tweet::get();
+        $tweets = Tweet::with('user')->paginate(2);
 
         return view('livewire.show-tweets',compact('tweets'));
+    }
+
+    public function create()
+    {
+
+        $this->validate();
+        Tweet::create([
+            'content'=> $this->content,
+            'user_id'=> 1,
+        ]);
+
+        $this->content = '';
     }
 }
