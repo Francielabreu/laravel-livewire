3149a27db4b2097a8fc9ec650604033c050d5960
= <=>
2022-10-13 17:03:50 -0300

    style : Melhorias no visual da aplicação usando o Tailwind CSS

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,37 +1,42 @@
 <div>
-    <h1>show tweets</h1>
+    <h1 class="text-4xl py-6 font-bold h-24 px-8">Tweets</h1>
 
-    <p>{{ $content }}</p>
-
-    <form method="post" wire:submit.prevent="create">
-        <input type="text" name="content" id="content" wire:model="content">
-
+    <form method="post" wire:submit.prevent="create" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
+        <label class="block text-gray-700 text-sm font-bold mb-4" for="username">
+            Tweet
+        </label>
+        <textarea name="content" id="content" rows="5" placeholder="O que está pensando?" wire:model="content"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('content') border-red-500 @enderror"></textarea>
         @error('content')
-            {{ $message }}
+            <p><span class="text-red-500">{{ $message }}</span></p>
         @enderror
-        <button type="submit">Criar Tweet</button>
+        <button type="submit" class="bg-blue-500 hover:bg-blue-400 text-white p-2 pl-6 pr-6 rounded">Tweetar</button>
     </form>
-    
-<hr>
+
     @foreach ($tweets as $tweet)
-        @if ($tweet->user->photo)
-        <img src="{{ url("storage/{$tweet->user->photo}") }}" width="70px" height="70px" alt="">
-        @else
-            <img src="{{ url('image/users.png') }}" width="70px" height="70px" alt="">
-        @endif
-
-        {{ $tweet->user->name }} - {{ $tweet->content }}
-
-        @if($tweet->likes->count())
-            <a href="#" wire:click.prevent="unlike({{ $tweet->id }})">Deslike</a>
-        @else
-        <a href="#" wire:click.prevent="like({{ $tweet->id }})">Like</a>
-        @endif
-        <br>
+        <div class="flex m-8 bg-white shadow-md rounded p-4">
+            <div class="w-1/8 pl-3 text-center">
+                @if ($tweet->user->photo)
+                    <img src="{{ url("storage/{$tweet->user->photo}") }}" alt="{{ $tweet->user->name }}"
+                        class="rounded-full h-8 w-8">
+                @else
+                    <img src="{{ url('image/usuario.png') }}" alt="{{ $tweet->user->name }}" class="rounded-full h-8 w-8">
+                @endif
+                {{-- $tweet->user->name --}}
+            </div>
+            <div class="w-7/8 pl-3 inline-block align-text-middle">
+                {{ $tweet->content }}
+                (@if ($tweet->likes->count())
+                    <a href="#" wire:click.prevent="unlike({{ $tweet->id }})"
+                        class="text-red-500">Descurtir</a>
+                @else
+                    <a href="#" wire:click.prevent="like({{ $tweet->id }})" class="text-teal-500">Curtir</a>
+                @endif)
+            </div>
+        </div>
     @endforeach
-</div>
 
-<hr>
-<div>
-    {{ $tweets->links() }}
+    <div class="py-12">
+        {{ $tweets->links() }}
+    </div>
 </div>
--- resources/views/livewire/user/upload-photo.blade.php
+++ resources/views/livewire/user/upload-photo.blade.php
@@ -1,11 +1,17 @@
 <div>
-    <h1>Atualizar Perfil</h1>
-    <form action="#" method="post" wire:submit.prevent="storagePhoto">
-        <input type="file" wire:model="photo">
+    <h1 class="text-4xl py-7 font-bold px-8">Atualizar Foto do Perfil</h1>
+    <form wire:submit.prevent="storagePhoto" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 m-8">
+        @if ($photo)
+            <div class="mb-4">
+                <img src="{{ $photo->temporaryUrl() }}" style="max-width: 200px;">
+            </div>
+        @endif
+        <div class="mb-4">
+            <input type="file" wire:model="photo">
+        </div>
 
-        @error('photo')
-            {{ $message }}
-        @enderror
-        <button type="submit">Upload</button>
+        @error('photo') <p><span class="text-red-500">{{ $message }}</span></p> @enderror
+
+        <button type="submit" class="bg-green-500 hover:bg-green-400 text-white p-2 pl-6 pr-6 rounded">Upload Foto</button>
     </form>
 </div>
