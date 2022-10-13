2b77f9923c2ec3cbf6783e670d8fb363f11164bb
= <=>
2022-10-13 10:50:17 -0300

    feat: Implementação da view tweets

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,11 +1,24 @@
 <div>
     <h1>show tweets</h1>
 
-    <p>{{ $message }}</p>
+    <p>{{ $content }}</p>
 
-    <input type="text" name="message" id="message" wire:model="message">
+    <form method="post" wire:submit.prevent="create">
+        <input type="text" name="content" id="content" wire:model="content">
+
+        @error('content')
+            {{ $message }}
+        @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
+    
 <hr>
     @foreach ($tweets as $tweet)
         {{ $tweet->user->name }} - {{ $tweet->content }}<br>
     @endforeach
+</div>
+
+<hr>
+<div>
+    {{ $tweets->links() }}
 </div>
